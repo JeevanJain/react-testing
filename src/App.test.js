f360ac327da9952e26fb71d19a17054f74142e10
Jeevan <JeevanJain@users.noreply.github.com>
2024-07-22 11:00:05 +0000

    complete app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
+test('can receive a new user and show it on list', async () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+
+  const user = userEvent.setup();
+
+  const nameInput = screen.getByRole('textbox', {
+    name: /name/i
+  });
+  const emailInput = screen.getByRole('textbox', {
+      name: /email/i
+  });
+
+  await user.click(nameInput);
+  await user.keyboard("jeevan");
+
+  await user.click(emailInput);
+  await user.keyboard("jeevantj@altimetrik.com");
+
+  const btn = screen.getByRole('button');
+
+  await user.click(btn);
+
+  // screen.debug();
+
+  const name = screen.getByRole('cell', { name : "jeevan"});
+  const email = screen.getByRole('cell', { name : "jeevantj@altimetrik.com" });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
