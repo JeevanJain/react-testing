29e7c4e8a92026b17f2d2721b5b428e31d9cefa6
Jeevan <JeevanJain@users.noreply.github.com>
2024-07-22 10:41:18 +0000

    check each rows

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -21,6 +21,20 @@ test('render one row per user', async () => {
 
 });
 
-test('', async () => {
+test('render the email and name of each user', async () => {
+
+    const users = [
+        {name: "Jeevan", email: "jeevan@gmail.com"},
+        {name: "jain", email: "jain@email.com"},
+    ];
+    render(<UserList users={users}/>);
+
+    for(let user of users) {
+        const name = screen.getByRole('cell', { name : user.name })
+        const email = screen.getByRole('cell', { name : user.email })
+
+        expect(name).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
+    }
 
 });
