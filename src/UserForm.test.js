6b4131ee134e2a018fd2d9059da5a9cb1775c00f
Jeevan <JeevanJain@users.noreply.github.com>
2024-07-22 06:03:02 +0000

    user form test

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react"
 import UserForm from "./UserForm"
-
+import userEvent from "@testing-library/user-event";
 
 test('it shows two inputs and a button', () => {
     render(<UserForm />);
@@ -10,4 +10,29 @@ test('it shows two inputs and a button', () => {
 
     expect(inputs).toHaveLength(2);
     expect(btn).toBeInTheDocument();
+})
+
+test('it calls onUserAdd when form is. submitted', async () => {
+    
+    const argList = [];
+    const callback = (...args) => {
+        argList.push(args);
+    }
+    render(<UserForm onUserAdd={callback} />);
+    const user = userEvent.setup();
+
+    const [nameInput, emailInput] = screen.getAllByRole('textbox');
+    const btn = screen.getByRole('button');
+
+    await user.click(nameInput);
+    await user.keyboard("jeevan");
+
+    await user.click(emailInput);
+    await user.keyboard("jeevantj@altimetrik.com");
+
+    await user.click(btn);
+
+    expect(argList).toHaveLength(1);
+    expect(argList[0][0]).toEqual({name: "jeevan", email: "jeevantj@altimetrik.com" });
+
 })
